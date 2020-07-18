fbb01d8cc4e8423f51bf0a4ced761472da2d8dfb
callthecops <tudor_rimbetiu@yahoo.com>
2020-07-18 23:35:41 +0300

    final

--- src/main/java/com/example/roomManager/controller/BedController.java
+++ src/main/java/com/example/roomManager/controller/BedController.java
@@ -10,8 +10,13 @@ import com.example.roomManager.utils.ResponseMsg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BedController {
@@ -55,4 +60,25 @@ public class BedController {
         return ResponseEntity.ok(result);
         //return "redirect:/home";
     }
+
+
+    @GetMapping("/retrievebeds")
+    @ResponseBody
+    public int retrieveTotalBeds() {
+        List<Bed> bedList = bedRepository.findAll();
+
+        int totalBeds = bedList.size();
+
+        return totalBeds;
+    }
+
+    @GetMapping("/retrievefreebeds")
+    @ResponseBody
+    public int retrieveFreeBeds() {
+        List<Bed> freeBeds = bedRepository.findAllByOccupiedFalse();
+        int freeBedsNumber = freeBeds.size();
+
+        return freeBedsNumber;
+    }
+
 }
--- src/main/java/com/example/roomManager/repository/BedRepository.java
+++ src/main/java/com/example/roomManager/repository/BedRepository.java
@@ -4,6 +4,11 @@ import com.example.roomManager.model.Bed;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BedRepository extends CrudRepository<Bed,Long> {
+public interface BedRepository extends CrudRepository<Bed, Long> {
+    @Override
+    List<Bed> findAll();
+    List<Bed> findAllByOccupiedFalse();
 }
